1bae43e95b7db77c802945ef03cb611a30cdec8c
Your Name <you@example.com>
2024-09-03 20:50:36 -0300

    Fire Base ja enviado

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation(platform("com.google.firebase:firebase-bom:33.2.0"))
-    implementation("com.google.firebase:firebase-analytics")
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
 
 }
