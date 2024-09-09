4aebe8327944577de3ac94be1d32c55120228cb8
Your Name <you@example.com>
2024-09-09 20:58:09 -0300

    Retirada e adição de Pontos e de Lobitos

---
+++ app/src/main/java/com/example/lobosnet/tira_ou_adiconar_ponto.java
@@ -0,0 +1,24 @@
+package com.example.lobosnet;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class tira_ou_adiconar_ponto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_tira_ou_adiconar_ponto);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
